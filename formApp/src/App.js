d525c8aa866852e12bf75f8b7dc22e2975ca87bb
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-03-03 14:55:51 +0530

    added radio btn & dropdown

--- formApp/src/App.js
+++ formApp/src/App.js
@@ -17,55 +17,125 @@ function App() {
   //   setLastName(event.target.value);
   // }
 
-  const [formData, setFormData] = useState({ firstName: "", lastName: "" ,email:"" ,comments:"", isVisible: true});
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    comments: "",
+    isVisible: true,
+    mode: "",
+    favCar: "",
+  });
 
   // console.log(formData.email);
-  console.log(formData)
+  console.log(formData);
 
-  function changeHandler (event){
-    const {name,value, checked, type} = event.target
-    setFormData(prevFormData =>{
-      return{
+  function changeHandler(event) {
+    const { name, value, checked, type } = event.target;
+    setFormData((prevFormData) => {
+      return {
         ...prevFormData,
-      [name]: value ==="checkbox" ? checked: value
-      }
+        [name]: value === "checkbox" ? checked : value,
+      };
     });
   }
-
 
   return (
     <div className="App">
       <form>
         <input
-          type="text" name="firstName" 
+          type="text"
+          name="firstName"
           placeholder="First name"
-          onChange={changeHandler} value={formData.firstName}
+          onChange={changeHandler}
+          value={formData.firstName}
         ></input>
 
         <br></br>
         <br></br>
         <input
-          type="text" name="lastName" 
+          type="text"
+          name="lastName"
           placeholder="Last name"
           onChange={changeHandler}
-        value={formData.lastName} ></input>
+          value={formData.lastName}
+        ></input>
 
         <br></br>
         <br></br>
-        <input type="email" name="email" placeholder="Email" onChange={changeHandler} value={formData.email}></input>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={changeHandler}
+          value={formData.email}
+        ></input>
 
         <br></br>
         <br></br>
-        <textarea placeholder="Enter Your Comments here..." onChange={changeHandler} name="comments" value={formData.comments}>
-
-        </textarea>
+        <textarea
+          placeholder="Enter Your Comments here..."
+          onChange={changeHandler}
+          name="comments"
+          value={formData.comments}
+        ></textarea>
 
         <br></br>
         <br></br>
 
-        <input type="checkbox" onChange={changeHandler} name="isVisible" checked={formData.isVisible}></input>
+        <input
+          type="checkbox"
+          onChange={changeHandler}
+          name="isVisible"
+          checked={formData.isVisible}
+        ></input>
 
         <label htmlFor="isVisible">Am I Visble ?</label>
+
+        <br></br>
+        <br></br>
+        <fieldset>
+          <legend> Mode:</legend>
+          <input
+            type="radio"
+            onChange={changeHandler}
+            name="mode"
+            value={"Online Mode"}
+            id="Online-Mode"
+          ></input>
+          <label
+            htmlFor="Online-Mode"
+            checked={formData.mode === "Online-Mode"}
+          >
+            Online Mode
+          </label>
+          <input
+            type="radio"
+            onChange={changeHandler}
+            name="mode"
+            value={"Offline Mode"}
+            id="Offline-Mode"
+            checked={formData.mode === "Online-Mode"}
+          ></input>
+          <label htmlFor="Offline-Mode">Offline Mode</label>
+        </fieldset>
+
+        <label htmlFor="favCar">Tell me your favourite car</label>
+        <select
+          name="favCar"
+          onChange={changeHandler}
+          id="favCar"
+          value={formData.favCar}
+        >
+          <option value={"scorpio"}>scorpio</option>
+          <option value={"fortuner"}>fortuner</option>
+          <option value={"thar"}>thar</option>
+          <option value={"legender"}>legender</option>
+          <option value={"defender"}>defender</option>
+        </select>
+
+        <br></br>
+        <br></br>
       </form>
     </div>
   );
