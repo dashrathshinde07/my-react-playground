951027392730e916f2157a5d54c3d3b37d52792c
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-03-12 14:29:29 +0530

    #

--- 1-todo-app-version-one/src/App.jsx
+++ 1-todo-app-version-one/src/App.jsx
@@ -2,30 +2,18 @@ import "./App.css";
 import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
 import TodoItem from "./components/TodoItem";
+import TodoItem2 from "./components/TodoItem2";
 
 function App() {
-
-
- 
   return (
     <center className="todo-container">
       <AppName />
 
       <AddTodo />
 
-      <TodoItem/>
+      <TodoItem />
 
-      <div className="container text-center">
-        <div className="row">
-          <div className="col-6">Go to college</div>
-          <div className="col-4">04/10/2023</div>
-          <div className="col-2">
-            <button type="button" className="btn btn-danger">
-              Delete
-            </button>
-          </div>
-        </div>
-      </div>
+      <TodoItem2 />
     </center>
   );
 }
---
+++ 1-todo-app-version-one/src/components/TodoItem2.jsx
@@ -0,0 +1,19 @@
+
+
+const TodoItem2 = () => {
+  return (
+    <div className="container text-center">
+    <div className="row">
+      <div className="col-6">Go to college</div>
+      <div className="col-4">04/10/2023</div>
+      <div className="col-2">
+        <button type="button" className="btn btn-danger">
+          Delete
+        </button>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default TodoItem2
