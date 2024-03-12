0ad2e64924d2c40c5723a168bc4fb0d98e413dfe
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-03-12 13:15:36 +0530

    #

---
+++ handson-on-bootstrap/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+import Alert from './components/Alert'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar/>
+      <Alert/>
+    </>
+  )
+}
+
+export default App
