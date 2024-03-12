2f43e8f59fb6716a605cc03d33dda2a181dd54f2
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-03-12 15:43:23 +0530

    #

---
+++ 2-bharat-clock-version-one/src/App.jsx
@@ -0,0 +1,23 @@
+
+import "./App.css";
+import ClockHeading from "./assets/components/ClockHeading";
+import ClockSlogan from "./assets/components/ClockSlogan";
+import CurrentTime from "./assets/components/CurrentTime";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+ 
+  return <center>
+
+
+    <ClockHeading/>
+    <ClockSlogan/>
+
+    <CurrentTime/>
+
+
+  </center>;
+}
+
+export default App;
---
+++ 2-bharat-clock-version-one/src/assets/components/ClockSlogan.jsx
@@ -0,0 +1,9 @@
+
+
+const ClockSlogan = () => {
+  return (
+    <div><p className="lead">This is the clock that shows the time in Bharat at all times.</p></div>
+  )
+}
+
+export default ClockSlogan
